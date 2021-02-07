fd7e2150ccc39e4a4b882aa62da8fffb27c80d61
Sejin Kim <jeen0202@korea.ac.kr>
2021-02-07 20:37:37 +0900

    Javascript의 Prototype

---
+++ JS/prototype.js
@@ -0,0 +1,23 @@
+/*prototype
+재사용성을 높힐 수 있다.
+*/ 
+
+function Person(name, f,s,t){
+    this.name=name,
+    this.first=f,
+    this.second=s,
+    this.third=t
+
+}
+//prototpye을 사용하면 멤버함수와는 다르게 객체 생성시마다 메모리를 할당받지 않게된다.
+//prototype으로 선언된 함수를 같은 메소드끼리 공유한다.
+Person.prototype.sum= function(){
+    return 'prototype : ' +(this.first+this.second+this.third);
+}
+var kim = new Person('kim',20,10,30);
+kim.sum= function(){
+    return 'this : ' +(this.first+this.second+this.third);
+}
+var lee = new Person('lee',10,10,20); 
+console.log(kim.sum())
+console.log(lee.sum())
