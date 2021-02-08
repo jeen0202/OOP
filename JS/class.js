ea823cf89d3fd0c58be9cc62e9618b5ceec72cf0
Sejin Kim <jeen0202@korea.ac.kr>
2021-02-08 16:27:36 +0900

    Class의 상속

--- JS/class.js
+++ JS/class.js
@@ -1,22 +1,30 @@
 class Person{
-    constructor(name, f, s, t){
+    constructor(name, f, s){
         console.log('constructor');
         this.name = name;
         this.first=f,
-        this.second=s,
-        this.third=t
+        this.second=s        
     }
     //prototype과 같은 역할을 해준다.
     sum(){
-        return 'prototype : ' +(this.first+this.second+this.third);
+        return (this.first+this.second);
+    }
+    
+}
+//extends 키워드를 통해 다른 Class를 상속받아 사용할 수 있다.
+class PersonPlus extends Person{
+    constructor(name,f,s,t){
+        super(name,f,s);
+        this.third=t;
+        console.log(this)
+    }
+    sum(){        
+        return super.sum()+this.third;
+    }
+    avg(){
+        return (this.sum())/3;
     }
 }
-//JavaScript의 class는 method의 연장선
-
-var kim = new Person('kim',10,20,30);
-var lee = new Person('lee',10,10,20); 
-kim.sum = function(){
-    return 'this : '+(this.first+this.second+this.third);
-}
-console.log(kim.sum())
-console.log(lee.sum())
+let go = new PersonPlus('go',15,15,15);
+console.log('go.sum() : ',go.sum());
+console.log('go.avg() : ',go.avg());
