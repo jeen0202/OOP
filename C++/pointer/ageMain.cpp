b1f6e513428224fb84fa10063c2dd46d06d7d909
Sejin Kim <jeen0202@korea.ac.kr>
2021-03-14 18:04:44 +0900

    복사 생성자 정리

--- C++/pointer/ageMain.cpp
+++ C++/pointer/ageMain.cpp
@@ -12,9 +12,10 @@ int main()
     cout << "boy의 나비 : " << boy.getAge() << endl;
 
     boy.setAge(15); // boy의 나이 변경
-    //복사 생성자를 만들지 않으면 기존객체인 girl의 멤버변수도 변경되어 버린다.
-   cout << "gitl의 나이 : " << girl.getAge() << endl;
+    //복사 생성자를 만들지 않으면 기존객체인 girl의 멤버변수도 변경되어 버린다. (얕은 복사)
+   cout << "girl의 나이 : " << girl.getAge() << endl;
    cout << "boy의 나비 : " << boy.getAge() << endl;
+   //girl의 멤버변수인 age는 변화하지 않는다. (복사생성자를 통한 깊은 복사)
 
    
 
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Study for Object Oriented Programming
+
+# C++
+
+### 복사 생성자
+Class의 복사가 이루어질때 호출되는 생성자
+스스로의 Class type 참조변수를 parameter로 가지며, overloading 될 수 없다.
+객체의 복사시에 호출되어 동적할당된 메모리를 공유하지 않고 복제하는 Deep copy를 가능하게 해준다.
