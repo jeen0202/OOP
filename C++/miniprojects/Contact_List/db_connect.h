ab08d668e40605efca8b36c6b17a1894410a24f2
Sejin Kim <jeen0202@korea.ac.kr>
2021-04-10 20:55:48 +0900

    기본 틀 구성

--- C++/miniprojects/Contact_List/db_connect.h
+++ C++/miniprojects/Contact_List/db_connect.h
@@ -6,23 +6,26 @@
 #include "mysql_driver.h"
 #include "mysql_connection.h"
 #include "mysql_error.h"
+#include <cppconn/exception.h>
 #include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
 
 #define DB_HOST "127.0.0.1"
 #define DB_USER "root"
 #define DB_PASS "111111"
-#define DB_NAME "tutorials"
-
+#define DB_NAME "oop"
 class db_connect
 {
 private:
     sql::mysql::MySQL_Driver *driver;
     sql::Connection *conn;
     sql::Statement *stmt;
+    sql::PreparedStatement *psmt;
     sql::ResultSet *res;
 public:
     db_connect(/* args */);
-    
+    void showList();
     ~db_connect();
 };
 
@@ -30,13 +33,25 @@ db_connect::db_connect(/* args */)
 {
     driver = sql::mysql::get_mysql_driver_instance();
     conn = driver->connect(DB_HOST,DB_USER,DB_PASS);
+    std::string use = "Use "DB_NAME;
+    stmt->execute(use);
 }
-
+void db_connect::showList()
+{
+    res = stmt->executeQuery("Select * FROM contact_list");
+    std::cout << "ID\t이름\t주소\t전화번호" << std::endl;
+    while(res->next())
+    {
+        std::cout << res->getInt("id") << "\t" << res->getString("name") << "\t" << res->getString("address")  
+        << " " << res->getString("phone") << std::endl;
+    }
+}
 db_connect::~db_connect()
 {
     delete res;
 	delete conn;
     delete stmt;
+    delete psmt;
 }
 
 #endif
