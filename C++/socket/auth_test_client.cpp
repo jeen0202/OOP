8aca3e0b20d76abcfa38d8af89d19ca5741884b5
Sejin Kim <jeen0202@korea.ac.kr>
2021-04-24 19:43:54 +0900

    인증기능 반복실행 구현

--- C++/socket/auth_test_client.cpp
+++ C++/socket/auth_test_client.cpp
@@ -10,24 +10,28 @@ int main(int argc, int argv[])
     {
         ClientSocket client_socket ("localhost", 30000);
         std::string reply;
-       
+        while(1)
+        {
             std:: cout << "Input Password >> ";
             std:: cin >> reply;
-        try{
-            
-                //Client에서 Server에 Message 전송                
-                client_socket << reply;
-                //Server에서 보내는 응답 수신
-                client_socket >> reply;
-                // if(reply=="Success"){
-                //     reply = "Login Success!";                    
-                // }else{
-                //     reply = "Fail";
-                // }
-            
-        }catch(SocketException&) {}        
-        std::cout << "Login " << reply <<"\''\n";;       
-    }
+            try{
+                
+                    //Client에서 Server에 Message 전송                
+                    client_socket << reply;
+                    //Server에서 보내는 응답 수신
+                    client_socket >> reply;
+                    // if(reply=="Success"){
+                    //     reply = "Login Success!";                    
+                    // }else{
+                    //     reply = "Fail";
+                    // }
+                
+            }catch(SocketException&) {}        
+            std::cout << "Login " << reply <<"\n";;  
+            if(reply=="Success")
+                break;     
+        }   
+}
     catch(SocketException& e)
     {
         std::cout << "Exception was caught :" << e.description() << std::endl;
